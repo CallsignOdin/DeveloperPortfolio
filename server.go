729849dc04e315b4dab051ab013751f8d256a200
Chester O <olesiakjoshua@gmail.com>
2023-04-08 11:30:45 -0500

    Created Gin web server
    Created gin web server and restructured files respectively i.e. templates/ and static/

---
+++ server.go
@@ -0,0 +1,23 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	// Create a new Gin router
+	router := gin.Default()
+
+	// Serve static files
+	router.Static("/css", "static/css")
+
+	// Load the HTML template
+	router.LoadHTMLGlob("templates/*")
+
+	// Define a route for the root path
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(200, "index.html", nil)
+
+	})
+
+	// Start the server on port 8080
+	router.Run(":8080")
+}
