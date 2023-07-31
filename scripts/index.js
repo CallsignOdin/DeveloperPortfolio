9b75198667062b8ff8b85fb4a82e9b0df663ffaf
Joshua O <olesiakjoshua@gmail.com>
2023-07-31 03:39:03 -0500

    Moved to static directory
    scripts directory unnecessary outside of static directory

--- scripts/index.js
+++
@@ -1,9 +0,0 @@
-
-function changeBgColor(btn) {
-    if(btn.style.backgroundColor == "green") {
-        btn.style.backgroundColor = "red";
-    }
-    else {
-        btn.style.backgroundColor = "green";
-    }  
-}
