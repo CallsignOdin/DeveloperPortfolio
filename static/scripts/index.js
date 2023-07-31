90fd0ec8aa0c420dd82d6bc75bb2d6ce21828da3
Joshua O <olesiakjoshua@gmail.com>
2023-07-31 03:35:41 -0500

    bugfix: stylesheet not loading properly
    webserver was serving old version of stylesheet, replaced with new stylesheet & saved original animation CSS.

---
+++ static/scripts/index.js
@@ -0,0 +1,9 @@
+
+function changeBgColor(btn) {
+    if(btn.style.backgroundColor == "green") {
+        btn.style.backgroundColor = "red";
+    }
+    else {
+        btn.style.backgroundColor = "green";
+    }  
+}
